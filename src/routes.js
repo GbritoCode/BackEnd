22522ef99721489945fbbf8f2359863ae20536c6
Gabriel <gabrielcabeca26@gmail.com>
2021-02-05 14:35:14 -0300

    teste

--- src/routes.js
+++ src/routes.js
@@ -46,32 +46,7 @@ import centroCustosController from './app/controllers/auxControllers/centroCusto
 
 const routes = new Router();
 
-const routesTeste = [
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    icon: 'tim-icons icon-chart-pie-36',
-    component: 'arr ? checkProfile(arr[23]) : null',
-    layout: '/admin',
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    mini: 'L',
-    component: 'signIn',
-    layout: '/auth',
-    redirect: true,
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    mini: 'R',
-    component: 'SignUp',
-    layout: '/auth',
-    redirect: true,
-  },
-];
-routes.get('/', (req, res) => res.send(routesTeste));
+routes.get('/', (req, res) => res.send('ok'));
 
 routes.post('/contaContabil', contaContabilController.store);
 routes.get('/contaContabil/:id?', contaContabilController.get);
