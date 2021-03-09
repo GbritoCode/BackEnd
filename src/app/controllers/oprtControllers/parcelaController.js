61c8ed72192fc50e6c9ec715fe424df814411a9a
Gabriel <gabrielcabeca26@gmail.com>
2021-03-09 18:57:01 -0300

    checar pipefy dia 09/03/2021

--- src/app/controllers/oprtControllers/parcelaController.js
+++ src/app/controllers/oprtControllers/parcelaController.js
@@ -113,7 +113,7 @@ class ParcelaController {
             }
 
             if (
-              moment(cli[i].Oportunidades[j].Parcelas[k].dtVencimento).isBefore(`${year}-${month + 1}-${date}`)
+              moment(`${year}-${month + 1}-${date}`).isAfter(cli[i].Oportunidades[j].Parcelas[k].dtVencimento)
              && cli[i].Oportunidades[j].Parcelas[k].situacao === 2
             ) {
               labelsAtrasada[parcAtrasadaCount] = cli[i].nomeAbv.slice(0, 3);
@@ -121,7 +121,7 @@ class ParcelaController {
               parcAtrasadaCount += 1;
               parcAtrasadaValue += cli[i].Oportunidades[j].Parcelas[k].vlrParcela;
             } if (
-              moment(cli[i].Oportunidades[j].Parcelas[k].dtVencimento).isAfter(`${year}-${month + 1}-${date}`)
+              (moment(`${year}-${month + 1}-${date}`).isAfter(cli[i].Oportunidades[j].Parcelas[k].dtVencimento))
               && cli[i].Oportunidades[j].Parcelas[k].situacao === 2
             ) {
               labelsAberta[parcAbertaCount] = cli[i].nomeAbv.slice(0, 3);
