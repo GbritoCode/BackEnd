45ee27057eb675f944ac944ef32fc0c1792bc736
Gabriel <gabrielcabeca26@gmail.com>
2021-05-14 09:59:49 -0300

    colocando url do mongo no .env

--- src/database/index.js
+++ src/database/index.js
@@ -87,7 +87,7 @@ class Database {
 
   mongo() {
     this.mongoConnection = mongoose.connect(
-      'mongodb://localhost:27017/tovo',
+      process.env.MONGO_URL,
       {
         useNewUrlParser: true,
         useFindAndModify: false,
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 import app from './app';
 
-app.listen(process.env.APP_PORT);
+app.listen(process.env.APP_PORT, () => console.log(process.env.MONGO_URL));
