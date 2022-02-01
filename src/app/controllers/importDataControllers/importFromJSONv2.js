99973b80b6de114121840b3672ed3812a30584a7
GbritoCode <70341944+GbritoCode@users.noreply.github.com>
2022-02-01 02:43:45 -0300

    adding Cli to prospectar
    adding Cli to prospectar

--- src/app/controllers/importDataControllers/importFromJSONv2.js
+++ src/app/controllers/importDataControllers/importFromJSONv2.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-restricted-syntax */
-import json from './json.json';
+import json from './teste.json';
 import Campanhas_Clientes from '../../models/Campanhas_Clientes';
 import CliCont from '../../models/cliCont';
 import Cliente from '../../models/cliente';
